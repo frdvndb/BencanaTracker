41f39b96f99a0580d6666e25e0fa070efa8a110e
Farid015 <farid32peb@gmail.com>
2023-06-07 01:25:49 +0800

    Reformat code

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Codeigniter 4 Show Multiple Markers on Google Map Example</title>
     <meta name="description" content="The tiny framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-      .container-fluid {
+    .container-fluid {
         max-width: 100%;
-      }
-      #gmapBlock {
-          width: 100%;
-          height: 100%;
-      }
-      .sidebar {
+    }
+
+    #gmapBlock {
+        width: 100%;
+        height: 100%;
+    }
+
+    .sidebar {
         font-family: "Saira Extra Condensed", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
         display: flex;
         flex-direction: column;
@@ -25,17 +28,17 @@
         font-size: 1.5rem;
         height: 100vh;
         position: relative;
-      }
+    }
 
-      .sidebar a {
+    .sidebar a {
         color: white;
-      }
+    }
 
-      .sidebar a:hover {
+    .sidebar a:hover {
         color: #00B4D8;
-      }
+    }
 
-      .content {
+    .content {
         display: flex;
         flex-direction: column;
         justify-content: flex-start;
@@ -51,38 +54,46 @@
         font-weight: 500;
         font-size: 1.5rem;
         color: #343a40;
-      }
+    }
 
-      h1 {
+    h1 {
         margin-top: 0;
         margin-bottom: 0.5rem;
         font-weight: 700;
         line-height: 1.2;
         font-size: calc(1.725rem + 5.7vw);
-      }
+    }
 
-      .text-primary {
+    .text-primary {
         --bs-primary-rgb: 189, 93, 56;
         --bs-text-opacity: 1;
         color: rgba(var(--bs-primary-rgb), var(--bs-text-opacity)) !important;
-      }
+    }
 
-      .img-profile {
+    .img-profile {
         border: 10px solid white;
         border-radius: 50%;
         width: 150px;
         height: 150px;
-      }
-      .h2w {color: white;}
-      .h2o {color: #FF5757;}
-      .laporButton {
+    }
+
+    .h2w {
+        color: white;
+    }
+
+    .h2o {
+        color: #FF5757;
+    }
+
+    .laporButton {
         background-color: white;
         color: #FF5757;
         font-weight: bold;
         width: 100%;
         margin-top: 40px;
-      }
-      .bottom-sidebar {
+    }
+
+    .bottom-sidebar {
         position: absolute;
         bottom: 20px;
         left: 0;
@@ -98,9 +109,9 @@
         background-color: #FF5757;
 
     }
-    
     </style>
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="row">
@@ -109,15 +120,17 @@
                 <h2><span class="h2w">Bencana</span><span class="h2o">Tracker</span></h2>
                 <ul class="nav flex-column mt-4">
                     <li class="nav-item">
-                        <a class="btn btn-primary laporButton" style="color: #FF5757;" href="<?= base_url('buat_laporan'); ?>">LAPORKAN<br> BENCANA</a>
+                        <a class="btn btn-primary laporButton" style="color: #FF5757;"
+                            href="<?= base_url('buat_laporan'); ?>">LAPORKAN<br> BENCANA</a>
                     </li>
                     <div class="main-sidebar">
-                    <li class="nav-item"><a class="nav-link" href="<?= base_url('beranda'); ?>">Beranda</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= base_url(''); ?>">Notifikasi</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= base_url('pencarianrelawan'); ?>">Pencarian Relawan</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= base_url(''); ?>">Histori Laporan</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= base_url('donasi'); ?>">Donasi</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= base_url('login'); ?>">Logout</a></li>
+                        <li class="nav-item"><a class="nav-link" href="<?= base_url('beranda'); ?>">Beranda</a></li>
+                        <li class="nav-item"><a class="nav-link" href="<?= base_url(''); ?>">Notifikasi</a></li>
+                        <li class="nav-item"><a class="nav-link" href="<?= base_url('pencarianrelawan'); ?>">Pencarian
+                                Relawan</a></li>
+                        <li class="nav-item"><a class="nav-link" href="<?= base_url(''); ?>">Histori Laporan</a></li>
+                        <li class="nav-item"><a class="nav-link" href="<?= base_url('donasi'); ?>">Donasi</a></li>
+                        <li class="nav-item"><a class="nav-link" href="<?= base_url('login'); ?>">Logout</a></li>
                     </div>
                     <div class="bottom-sidebar">
                         <li class="nav-item"><a class="nav-link" href="<?= base_url(''); ?>"><?= $username ?></a></li>
@@ -129,81 +142,84 @@
                 <div id="gmapBlock"></div>
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
                 <script>
-                    $(function() {
-                        var script = document.createElement('script');
-                        script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyA85_vUbvIQtqVIFFKYjESeKJpK_rr_rVg&sensor=false&callback=initialize";
-                        document.body.appendChild(script);
-                    });
-                    
-                    function initialize() {
-                        var map;
-                        var bounds = new google.maps.LatLngBounds();
-                        var mapOptions = {
-                            mapTypeId: 'roadmap'
-                        };
+                $(function() {
+                    var script = document.createElement('script');
+                    script.src =
+                        "https://maps.googleapis.com/maps/api/js?key=AIzaSyA85_vUbvIQtqVIFFKYjESeKJpK_rr_rVg&sensor=false&callback=initialize";
+                    document.body.appendChild(script);
+                });
 
-                        map = new google.maps.Map(document.getElementById("gmapBlock"), mapOptions);
-                        map.setTilt(45);
+                function initialize() {
+                    var map;
+                    var bounds = new google.maps.LatLngBounds();
+                    var mapOptions = {
+                        mapTypeId: 'roadmap'
+                    };
 
-                        var locationMarkers = JSON.parse(`<?php echo ($locationMarkers); ?>`);
+                    map = new google.maps.Map(document.getElementById("gmapBlock"), mapOptions);
+                    map.setTilt(45);
 
-                        var locInfo = JSON.parse(`<?php echo ($locInfo); ?>`);
+                    var locationMarkers = JSON.parse(`<?php echo ($locationMarkers); ?>`);
 
-                        var infoWindow = new google.maps.InfoWindow(), marker, i;
-                        var clickedMarker = null;
+                    var locInfo = JSON.parse(`<?php echo ($locInfo); ?>`);
 
-                        for( i = 0; i < locationMarkers.length; i++ ) {
-                            var position = new google.maps.LatLng(locationMarkers[i][1], locationMarkers[i][2]);
-                            bounds.extend(position);
-                            marker = new google.maps.Marker({
-                                position: position,
-                                map: map,
-                                title: locationMarkers[i][0]
-                            });
+                    var infoWindow = new google.maps.InfoWindow(),
+                        marker, i;
+                    var clickedMarker = null;
 
-                            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                                return function() {
-                                    infoWindow.setContent(locInfo[i][0]);
-                                    infoWindow.open(map, marker);
-                                }
-                            })(marker, i));
+                    for (i = 0; i < locationMarkers.length; i++) {
+                        var position = new google.maps.LatLng(locationMarkers[i][1], locationMarkers[i][2]);
+                        bounds.extend(position);
+                        marker = new google.maps.Marker({
+                            position: position,
+                            map: map,
+                            title: locationMarkers[i][0]
+                        });
 
-                            map.fitBounds(bounds);
-                        }
-
-                        google.maps.event.addListener(map, 'click', function(event) {
-                            var clickedLocation = event.latLng;
-                            var marker = new google.maps.Marker({
-                                position: clickedLocation,
-                                map: map,
-                                title: 'Clicked Location'
-                            });
-
-
-                            var locationName = "";
-                            var latitude = clickedLocation.lat();
-                            var longitude = clickedLocation.lng();
-
-                            if (latitude !== null && latitude !== "") {
-                                localStorage.setItem('clickedLocation', JSON.stringify({
-                                    latitude: latitude,
-                                    longitude: longitude,
-                                    locationName: locationName
-                                }));
-                                window.location.href = 'buat_laporan';
-                            } else {
-                                marker.setMap(null);
+                        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                            return function() {
+                                infoWindow.setContent(locInfo[i][0]);
+                                infoWindow.open(map, marker);
                             }
+                        })(marker, i));
+
+                        map.fitBounds(bounds);
+                    }
+
+                    google.maps.event.addListener(map, 'click', function(event) {
+                        var clickedLocation = event.latLng;
+                        var marker = new google.maps.Marker({
+                            position: clickedLocation,
+                            map: map,
+                            title: 'Clicked Location'
                         });
 
-                        var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-                            this.setZoom(5);
-                            google.maps.event.removeListener(boundsListener);
-                        });
-                    }
+
+                        var locationName = "";
+                        var latitude = clickedLocation.lat();
+                        var longitude = clickedLocation.lng();
+
+                        if (latitude !== null && latitude !== "") {
+                            localStorage.setItem('clickedLocation', JSON.stringify({
+                                latitude: latitude,
+                                longitude: longitude,
+                                locationName: locationName
+                            }));
+                            window.location.href = 'buat_laporan';
+                        } else {
+                            marker.setMap(null);
+                        }
+                    });
+
+                    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+                        this.setZoom(5);
+                        google.maps.event.removeListener(boundsListener);
+                    });
+                }
                 </script>
             </div>
         </div>
     </div>
 </body>
+
 </html>
