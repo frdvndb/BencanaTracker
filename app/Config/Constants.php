4e37c480412c324edf552d8282dd0b233bc9986a
Caldron47 <99594434+Caldron47@users.noreply.github.com>
2023-06-07 20:40:28 +0800

    dynamic base url

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -92,3 +92,7 @@ define('EVENT_PRIORITY_NORMAL', 100);
  * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_HIGH instead.
  */
 define('EVENT_PRIORITY_HIGH', 10);
+
+$base = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) : 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']);
+
+defined('BASE') || define('BASE', $base );
