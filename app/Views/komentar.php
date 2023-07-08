7b435e07bf44a33d210003f87a0eeac9e688db35
Farid015 <farid32peb@gmail.com>
2023-07-09 02:02:26 +0700

    update jika belum ada komentar

--- app/Views/komentar.php
+++ app/Views/komentar.php
@@ -11,226 +11,226 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <style>
-        .card {
-            margin: auto;
-            width: 30%;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-            border-radius: 5px;
-            padding: 20px;
-            position: relative;
-            top: 50%;
-            background-color: #1546BA;
-        }
+    .card {
+        margin: auto;
+        width: 30%;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        border-radius: 5px;
+        padding: 20px;
+        position: relative;
+        top: 50%;
+        background-color: #1546BA;
+    }
 
-        .container-fluid {
-            max-width: 100%;
-        }
+    .container-fluid {
+        max-width: 100%;
+    }
 
-        #gmapBlock {
-            width: 100%;
-            height: 100%;
-        }
+    #gmapBlock {
+        width: 100%;
+        height: 100%;
+    }
 
-        .sidebar {
-            font-family: "Saira Extra Condensed", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            background-color: #1546BA;
-            padding: 20px;
-            font-size: 1.2rem;
-            height: 100vh;
-            position: relative;
-        }
+    .sidebar {
+        font-family: "Saira Extra Condensed", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: #1546BA;
+        padding: 20px;
+        font-size: 1.2rem;
+        height: 100vh;
+        position: relative;
+    }
 
-        .sidebar a {
-            color: white;
-        }
+    .sidebar a {
+        color: white;
+    }
 
-        .sidebar a:hover {
-            color: #00B4D8;
-        }
+    .sidebar a:hover {
+        color: #00B4D8;
+    }
 
-        .content {
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-start;
-            align-items: flex-start;
-            height: 100vh;
-            padding: 20px;
-            background-color: #E5E5E5;
-            background-size: cover;
-            background-position: center;
-            text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);
-            font-family: "Saira Extra Condensed", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-            text-transform: uppercase;
-            font-weight: 500;
-            font-size: 1.5rem;
-            color: #343a40;
-        }
+    .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        height: 100vh;
+        padding: 20px;
+        background-color: #E5E5E5;
+        background-size: cover;
+        background-position: center;
+        text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);
+        font-family: "Saira Extra Condensed", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        text-transform: uppercase;
+        font-weight: 500;
+        font-size: 1.5rem;
+        color: #343a40;
+    }
 
-        .background-input {
-            background-color: #1546BA;
-            color: white;
-        }
+    .background-input {
+        background-color: #1546BA;
+        color: white;
+    }
 
-        button {
-            background-color: #1546BA;
-            color: white;
-            border-color: white;
-        }
+    button {
+        background-color: #1546BA;
+        color: white;
+        border-color: white;
+    }
 
-        .text-primary {
-            --bs-primary-rgb: 189, 93, 56;
-            --bs-text-opacity: 1;
-            color: rgba(var(--bs-primary-rgb), var(--bs-text-opacity)) !important;
-        }
+    .text-primary {
+        --bs-primary-rgb: 189, 93, 56;
+        --bs-text-opacity: 1;
+        color: rgba(var(--bs-primary-rgb), var(--bs-text-opacity)) !important;
+    }
 
-        .img-profile {
-            border: 10px solid white;
-            border-radius: 50%;
-            width: 150px;
-            height: 150px;
-        }
+    .img-profile {
+        border: 10px solid white;
+        border-radius: 50%;
+        width: 150px;
+        height: 150px;
+    }
 
-        .h2w {
-            color: white;
-        }
+    .h2w {
+        color: white;
+    }
 
-        .h2o,
-        h5 {
-            color: #FF5757;
-        }
+    .h2o,
+    h5 {
+        color: #FF5757;
+    }
 
-        .laporButton {
-            background-color: white;
-            color: #FF5757;
-            font-weight: bold;
-            width: 100%;
-            margin-top: 40px;
-        }
+    .laporButton {
+        background-color: white;
+        color: #FF5757;
+        font-weight: bold;
+        width: 100%;
+        margin-top: 40px;
+    }
 
-        .bottom-sidebar {
-            position: absolute;
-            bottom: 20px;
-            left: 0;
-            width: 100%;
-            text-align: center;
-            color: #FF5757;
-        }
+    .bottom-sidebar {
+        position: absolute;
+        bottom: 20px;
+        left: 0;
+        width: 100%;
+        text-align: center;
+        color: #FF5757;
+    }
 
-        .bottom-sidebar li {
-            border: 1px solid white;
-            width: 80%;
-            margin: 0 auto;
-            background-color: #FF5757;
-        }
+    .bottom-sidebar li {
+        border: 1px solid white;
+        width: 80%;
+        margin: 0 auto;
+        background-color: #FF5757;
+    }
 
-        .bottom-left-buttons {
-            position: absolute;
-            bottom: 20px;
-            left: 20px;
-            display: flex;
-            gap: 10px;
-            align-items: center;
-        }
+    .bottom-left-buttons {
+        position: absolute;
+        bottom: 20px;
+        left: 20px;
+        display: flex;
+        gap: 10px;
+        align-items: center;
+    }
 
-        .bottom-right-buttons {
-            position: absolute;
-            bottom: 20px;
-            right: 20px;
-            display: flex;
-            gap: 10px;
-            align-items: center;
-        }
+    .bottom-right-buttons {
+        position: absolute;
+        bottom: 20px;
+        right: 20px;
+        display: flex;
+        gap: 10px;
+        align-items: center;
+    }
 
-        body {
-            background-color: #E5E5E5;
-        }
+    body {
+        background-color: #E5E5E5;
+    }
 
-        /* Add this to your existing CSS styles */
+    /* Add this to your existing CSS styles */
 
-        .comments-section {
-            margin-top: 20px;
-        }
+    .comments-section {
+        margin-top: 20px;
+    }
 
-        .comments-section h3 {
-            font-size: 20px;
-            font-weight: bold;
-            margin-bottom: 10px;
-        }
+    .comments-section h3 {
+        font-size: 20px;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
 
-        .comment {
-            display: flex;
-            align-items: center;
-            margin-bottom: 10px;
-            justify-content: flex-start;
-        }
+    .comment {
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+        justify-content: flex-start;
+    }
 
-        .user-profile {
-            display: flex;
-            align-items: center;
-        }
+    .user-profile {
+        display: flex;
+        align-items: center;
+    }
 
-        .user-profile img {
-            width: 50px;
-            height: 50px;
-            border-radius: 50%;
-            margin-right: 10px;
-        }
+    .user-profile img {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
 
-        .user-details {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            color: white;
-        }
+    .user-details {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        color: white;
+    }
 
-        .comment-actions {
-            display: flex;
-            align-items: center;
-            margin-left: auto;
-        }
+    .comment-actions {
+        display: flex;
+        align-items: center;
+        margin-left: auto;
+    }
 
-        .comment-actions .like-count {
-            font-size: 14px;
-            color: white;
-            margin-left: 5px;
-        }
+    .comment-actions .like-count {
+        font-size: 14px;
+        color: white;
+        margin-left: 5px;
+    }
 
-        .like-button {
-            background-color: transparent;
-            border: none;
-            cursor: pointer;
-        }
+    .like-button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+    }
 
-        .like-button i {
-            color: red;
-        }
+    .like-button i {
+        color: red;
+    }
 
-        .like-count {
-            margin-left: 5px;
-            font-size: 14px;
-            color: white;
-        }
+    .like-count {
+        margin-left: 5px;
+        font-size: 14px;
+        color: white;
+    }
 
-        .add-comment textarea {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            resize: vertical;
-            margin-bottom: 10px;
-        }
+    .add-comment textarea {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        resize: vertical;
+        margin-bottom: 10px;
+    }
 
-        .add-comment .post-button {
-            background-color: #00CC99;
-            color: white;
-            border: none;
-            padding: 8px 20px;
-            border-radius: 5px;
-            cursor: pointer;
-        }
+    .add-comment .post-button {
+        background-color: #00CC99;
+        color: white;
+        border: none;
+        padding: 8px 20px;
+        border-radius: 5px;
+        cursor: pointer;
+    }
     </style>
 </head>
 
@@ -256,7 +256,7 @@
                         <li class="nav-item"><a class="nav-link" href="<?= base_url('histori_laporan'); ?>"><i
                                     class="bi bi-clock-history"></i> Histori Laporan</a></li>
                         <li class="nav-item"><a class="nav-link" href="<?= base_url('donasi'); ?>"><i
-                        class="bi bi-cash-stack"></i> Support Us</a></li>
+                                    class="bi bi-cash-stack"></i> Support Us</a></li>
                         <?php if (!$username == null) { ?>
                         <li class="nav-item"><a class="nav-link" href="<?= base_url('logout'); ?>"><i
                                     class="bi bi-box-arrow-right"></i> Logout</a></li>
@@ -283,30 +283,37 @@
             <div class="col-md-10 card">
                 <div class="comments-section">
                     <h3 style="text-align: center; color: white;">Komentar</h3>
-                    <?php foreach ($dataKomentar as $dk) {  ?>
-                        <div class="comment">
-                            <div class="user-profile">
-                                <img src="<?= base_url('../assets/img/background.jpg') ?>" alt="User Profile Picture">
-                                <div class="user-details" style="margin-left: 10px;">
-                                    <h5 class="h2o" style="margin-bottom: 0;"><?= $dk['username'] ?></h5>
-                                    <p style="color: white; margin-top: 5px;"><?= $dk['komentar'] ?></p>
-                                </div>
-                            </div>
-                            <div class="comment-actions">
-                                <button class="like-button"><i class="far fa-calendar-alt"></i></button>
-                                <span class="like-count"><?= $dk['tanggal'] ?></span>
+                    <?php if ($dataKomentar == NULL) { ?>
+                    <div class="comment" style="color: white; font-weight: 100;">Belum ada komentar...</div>
+
+                    <?php } else {
+                    foreach ($dataKomentar as $dk) {  ?>
+                    <div class="comment">
+                        <div class="user-profile">
+                            <img src="<?= base_url('../assets/img/background.jpg') ?>" alt="User Profile Picture">
+                            <div class="user-details" style="margin-left: 10px;">
+                                <h5 class="h2o" style="margin-bottom: 0;"><?= $dk['username'] ?></h5>
+                                <p style="color: white; margin-top: 5px;"><?= $dk['komentar'] ?></p>
                             </div>
                         </div>
-                    <?php } ?>
-                    <form action="<?= base_url('komentar/'.$dataLaporan['id']) ?>" method="POST">
-                    <div class="add-comment">
-                        <textarea class="comment-input" name="komentar" placeholder="Tulis komentar..." style="color: black;"></textarea>
-                        <button class="post-button background-proses">Post</button>
+                        <div class="comment-actions">
+                            <button class="like-button"><i class="far fa-calendar-alt"></i></button>
+                            <span class="like-count"><?= $dk['tanggal'] ?></span>
+                        </div>
                     </div>
+                    <?php } 
+                     } 
+            }?>
+                    <form action="<?= base_url('komentar/'.$dataLaporan['id']) ?>" method="POST">
+                        <div class="add-comment">
+                            <textarea class="comment-input" name="komentar" placeholder="Tulis komentar..."
+                                style="color: black;"></textarea>
+                            <button class="post-button background-proses">Post</button>
+                        </div>
                     </form>
                 </div>
             </div>
-            <?php } ?>
+
         </div>
     </div>
 </body>
